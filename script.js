9efdfb5b752e7129591af572edd5c31d74394af6
AbdulhakimUS <127390486+AbdulhakimUS@users.noreply.github.com>
2025-09-21 16:33:39 +0500

    change

---
+++ script.js
@@ -0,0 +1,10 @@
+// Scroll effect navbar
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
